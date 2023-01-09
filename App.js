550cad79f50e708fafcaf8fb5d66adaaf1f4d25d
Josue Ttito <josue@MacBook-Pro.local>
2023-01-09 17:36:53 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const ws = useRef(null);
+
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8956/ws/tracking/asdf/");
+
+    ws.current.onopen = () => {
+      console.log('WebSocket is open');
+    };
+
+    ws.current.onmessage = (e) => {
+      console.log('Received message: ', e.data);
+    };
+
+    ws.current.onerror = (e) => {
+      console.log('WebSocket error: ', e.message);
+    };
+
+    ws.current.onclose = (e) => {
+      console.log('WebSocket closed: ', e.code, e.reason);
+    };
+
+    return () => {
+      ws.current.close();
+    };
+
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
